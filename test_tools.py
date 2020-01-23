fad224aff09ef823f3400f01c3facacf80111466
igor-morawski <igor.morawski@hotmail.com>
2020-01-23 14:42:49 +0800

    examples+new functions+tests

--- test_tools.py
+++ test_tools.py
@@ -1,84 +1,194 @@
-import unittest 
+import unittest
 import numpy as np
+import pandas as pd
 import os
+import cv2
 
 import tools
 
-SAMPLE_FP = os.path.join("testing", "sample.TXT")
-FRAMES_EXPECTED_FP = os.path.join("testing", "expected.npy")
-TMP_PATH = os.path.join("TMP")
+EXPECTED_TXT_FP = os.path.join("testing", "expected.TXT")
+EXPECTED_NP_FP = os.path.join("testing", "expected.npy")
+EXPECTED_CSV_FP = os.path.join("testing", "expected.csv")
+TMP_PATH = os.path.join("testing", "TMP")
+
 
 def _init():
-    if os.path.exists(TMP_PATH):
-        os.rmdir(TMP_PATH)
-    os.mkdir(TMP_PATH)
+    if not os.path.exists(TMP_PATH):
+        os.mkdir(TMP_PATH)
 
-def _cleanup():
+
+def _cleanup(files_fp: list = None):
+    if files_fp:
+        for fp in files_fp:
+            os.remove(fp)
     if os.path.exists(TMP_PATH):
         os.rmdir(TMP_PATH)
+
 
 class TestTxt2np(unittest.TestCase):
     def test_Result(self):
         expected_frames_shape = (3, 32, 32)
         expected_timestamps = [170.093, 170.218, 170.343]
-        result = tools.txt2np(filepath = SAMPLE_FP, array_size = 32)
+        result = tools.txt2np(filepath=EXPECTED_TXT_FP, array_size=32)
         frames, timestamps = result
         self.assertEqual(timestamps, expected_timestamps)
         self.assertEqual(frames.shape, expected_frames_shape)
-        expected_frames = np.load(FRAMES_EXPECTED_FP)
+        expected_frames = np.load(EXPECTED_NP_FP)
         self.assertTrue(np.array_equal(frames, expected_frames))
+
     def test_Defaults(self):
         """
         Default tested: array_size = 32
         """
-        expected = tools.txt2np(filepath = SAMPLE_FP, array_size = 32)
-        result = tools.txt2np(filepath = SAMPLE_FP)
+        expected = tools.txt2np(filepath=EXPECTED_TXT_FP, array_size=32)
+        result = tools.txt2np(filepath=EXPECTED_TXT_FP)
         expected_frames, expected_timestamps = expected
         frames, timestamps = result
         self.assertTrue(np.array_equal(frames, expected_frames))
+
     def test_DTYPE(self):
         expected_dtype = np.dtype(tools.DTYPE)
-        result = tools.txt2np(filepath = SAMPLE_FP)
+        result = tools.txt2np(filepath=EXPECTED_TXT_FP)
         frames, timestamps = result
         self.assertEqual(frames.dtype, expected_dtype)
+
     def test_array_size(self):
         expected_frames_shape = (3, 16, 16)
         expected_timestamps = [170.093, 170.218, 170.343]
-        result = tools.txt2np(filepath = SAMPLE_FP, array_size = 16)
+        result = tools.txt2np(filepath=EXPECTED_TXT_FP, array_size=16)
         frames, timestamps = result
         self.assertEqual(timestamps, expected_timestamps)
         self.assertEqual(frames.shape, expected_frames_shape)
 
-class TestNp2df(unittest.TestCase):
+
+class TestnpWriteCsv(unittest.TestCase):
     def test_Result(self):
-        input = np.load(FRAMES_EXPECTED_FP)
-        # TODO: unit testing
-        pass
+        _init()
+        expected_array = np.load(EXPECTED_NP_FP)
+        timestamps = [170.093, 170.218, 170.343]
+        csv_fp = os.path.join(TMP_PATH, "file.csv")
+        tools.npWriteCsv(csv_fp, expected_array, timestamps)
+        self.assertTrue(os.path.exists(csv_fp))
+        df = pd.read_csv(csv_fp, **tools.READ_CSV_ARGS)
+        timestamps = df[tools.PD_TIME_COL]
+        array = df.drop([tools.PD_TIME_COL, tools.PD_PTAT_COL], axis=1).to_numpy(
+            dtype=tools.DTYPE
+        )
+        array = tools.reshapeFlattenedFrames(array)
+        self.assertTrue(np.array_equal(array, expected_array))
+        _cleanup([csv_fp])
+
+
+class TestpcWriteGif(unittest.TestCase):
+    def test_Defaults(self):
+        _init()
+        gif_fp = os.path.join(TMP_PATH, "tmp.gif")
+        temperature_array = np.load(EXPECTED_NP_FP)
+        pc_array = tools.np2pc(temperature_array)
+        tools.pcWriteGif(pc_array, gif_fp)
+        self.assertTrue(os.path.exists(gif_fp))
+        # TODO read back the gif?
+        _cleanup([gif_fp])
+
+    def test_DurationAndLoop(self):
+        _init()
+        gif_fp = os.path.join(TMP_PATH, "tmp.gif")
+        temperature_array = np.load(EXPECTED_NP_FP)
+        pc_array = tools.np2pc(temperature_array)
+        expected_timestamps = [1.424, 2.453453, 3.5345]
+        duration_list = [
+            x_t2 - x_t1
+            for x_t1, x_t2 in zip(expected_timestamps, expected_timestamps[1:])
+        ]
+        duration_list.append(duration_list[-1])
+        tools.pcWriteGif(pc_array, gif_fp, duration=duration_list, loop=2)
+        self.assertTrue(os.path.exists(gif_fp))
+        # TODO read back the gif?
+        # TODO cleanup
+
 
 class TestflattenFrames(unittest.TestCase):
     def test_Result(self):
         input_shape = (100, 32, 32)
-        expected_shape = (100, 32*32)
+        expected_shape = (100, 32 * 32)
         input = np.arange(np.prod(input_shape)).reshape(input_shape)
         result = tools.flattenFrames(input)
         self.assertEqual(result.shape, expected_shape)
-        
+
+
+class TestapplyHeatmap(unittest.TestCase):
+    def test_Result(self):
+        array = np.ones([2, 40, 40])
+        array[0, :, :] = np.zeros([40, 40])
+        # default color maps is cv2.:COLORMAP_JET
+        result = tools.applyHeatmap(array)
+        self.assertTrue(np.array_equal(result[0, 0, 0], [128, 0, 0]))
+        self.assertTrue(np.array_equal(result[1, 0, 0], [0, 0, 128]))
+
+    def test_Colormaps(self):
+        array = np.ones([2, 40, 40])
+        array[0, :, :] = np.zeros([40, 40])
+        # cv2.COLORMAP_HOT = 11
+        result = tools.applyHeatmap(array, cv_colormap=11)
+        self.assertTrue(np.array_equal(result[0, 0, 0], [0, 0, 0]))
+        self.assertTrue(np.array_equal(result[1, 0, 0], [255, 255, 255]))
+
+
+class TestNp2pc(unittest.TestCase):
+    def test_Result(self):
+        array = np.ones([2, 40, 40])
+        array[0, :, :] = np.zeros([40, 40])
+        # default color maps is cv2.:COLORMAP_JET
+        result = tools.np2pc(array)
+        self.assertTrue(np.array_equal(result[0, 0, 0], [128, 0, 0]))
+        self.assertTrue(np.array_equal(result[1, 0, 0], [0, 0, 128]))
+
+    def test_Colormaps(self):
+        array = np.ones([2, 40, 40])
+        array[0, :, :] = np.zeros([40, 40])
+        # cv2.COLORMAP_HOT = 11
+        result = tools.np2pc(array, cv_colormap=11)
+        self.assertTrue(np.array_equal(result[0, 0, 0], [0, 0, 0]))
+        self.assertTrue(np.array_equal(result[1, 0, 0], [255, 255, 255]))
+
+
+class TestSaveFrames(unittest.TestCase):
+    def test_Files(self):
+        _init()
+        height, width = 100, 100
+        zeros = np.zeros([height, width], dtype=np.uint8)
+        ones = 255 * np.ones([height, width], dtype=np.uint8)
+        b = np.stack([ones, zeros, zeros], axis=-1)
+        g = np.stack([zeros, ones, zeros], axis=-1)
+        r = np.stack([zeros, zeros, ones], axis=-1)
+        array = np.stack([b, g, r])
+        tools.saveFrames(array, TMP_PATH)
+        fp_list = []
+        for idx in range(3):
+            fp = os.path.join(TMP_PATH, str(idx) + ".bmp")
+            fp_list.append(fp)
+            self.assertTrue(os.path.exists(fp))
+            img = cv2.imread(fp)
+            self.assertTrue(np.array_equal(img, array[idx]))
+        _cleanup(fp_list)
+
+
 class TestreshapeFlattenedFrames(unittest.TestCase):
     def test_Result(self):
-        input_shape = (100, 32*32)
+        input_shape = (100, 32 * 32)
         expected_shape = (100, 32, 32)
         input = np.arange(np.prod(input_shape)).reshape(input_shape)
         result = tools.reshapeFlattenedFrames(input)
         self.assertEqual(result.shape, expected_shape)
 
+
 class TestreshapingFrames(unittest.TestCase):
     def testflattenAndReshape(self):
-        input = np.load(FRAMES_EXPECTED_FP)
-        expected_frames_shape = (3, 32, 32)
-        expectedflattened_frames_shape = (3, 32**2)
+        input = np.load(EXPECTED_NP_FP)
         flattened_result = tools.flattenFrames(input)
         first_frame = flattened_result[0]
         expected_first_frame = input[0].flatten()
         self.assertTrue(np.array_equal(first_frame, expected_first_frame))
         reshaped_result = tools.reshapeFlattenedFrames(flattened_result)
         self.assertTrue(np.array_equal(input, reshaped_result))
+
--- tools.py
+++ tools.py
@@ -1,16 +1,34 @@
 """
 Tools for Heimann HTPA recordings. 
+
+Data types:
+    * txt - Heimann HTPA recordings in *.TXT,
+    * np - NumPy array of thermopile sensor array data, shaped [frames, height, width],
+    * csv - thermopile sensor array data in *.csv format, NOT a pandas dataframe!
+    * df - pandas dataframe
+    * pc - NumPy array of pseudocolored thermopile sensor array data, shaped [frames, height, width, channels]
 """
 import numpy as np
 import pandas as pd
-DTYPE = 'float32'
-PD_SEP = ';'
+import cv2
+import os
+import imageio
+
+DTYPE = "float32"
+PD_SEP = ","
 PD_NAN = np.inf
 PD_DTYPE = np.float32
+READ_CSV_ARGS = {"skiprows": 1}
+PD_TIME_COL = "Time (sec)"
+PD_PTAT_COL = "PTAT"
 
-def txt2np(filepath: str, array_size:int=32):
+# TODO: np -> array (naming convention)
+# TODO: 2 -> write, if not conversion
+
+
+def txt2np(filepath: str, array_size: int = 32):
     """
-    Convert Heimann HTPA .txt to NumPy array shaped [frame, height, width].
+    Convert Heimann HTPA .txt to NumPy array shaped [frames, height, width].
 
     Parameters
     ----------
@@ -20,12 +38,12 @@ def txt2np(filepath: str, array_size:int=32):
     Returns
     -------
     np.array
-        3D array of temperature distribution sequence, shaped [frame, height, width].
+        3D array of temperature distribution sequence, shaped [frames, height, width].
     """
     with open(filepath) as f:
-        #discard the first line
+        # discard the first line
         _ = f.readline()
-        #read line by line now
+        # read line by line now
         line = "dummy line"
         frames = []
         timestamps = []
@@ -33,58 +51,145 @@ def txt2np(filepath: str, array_size:int=32):
             line = f.readline()
             if line:
                 split = line.split(" ")
-                frame = split[0:array_size**2]
+                frame = split[0 : array_size ** 2]
                 timestamp = split[-1]
                 frame = np.array([int(T) for T in frame], dtype=DTYPE)
-                frame = frame.reshape([array_size, array_size], order='F')
+                frame = frame.reshape([array_size, array_size], order="F")
                 frame *= 1e-2
                 frames.append(frame)
                 timestamps.append(float(timestamp))
         frames = np.array(frames)
         # the array needs rotating 90 CW
-        frames = np.rot90(frames, k=-1, axes = (1, 2))
+        frames = np.rot90(frames, k=-1, axes=(1, 2))
     return frames, timestamps
 
-def np2df(output_fp:str, array, timestamps: list):
+
+def npWriteCsv(output_fp: str, array, timestamps: list) -> bool:
     """
-    Convert Heimann HTPA NumPy array shaped [frame, height, width] to .CSV dataframe
+    Convert and save Heimann HTPA NumPy array shaped [frames, height, width] to .CSV dataframe
 
     Parameters
     ----------
     output_fp : str
         Filepath to destination file, including the file name.
     array : np.array
-        Temperatue distribution sequence, shape [frame, height, width].
+        Temperatue distribution sequence, shaped [frames, height, width].
     timestamps : list
         List of timestamps of corresponding array frames.
     """
     # initialize csv template (and append frames later)
     # prepend first row for compability with legacy format
-    first_row = pd.DataFrame({'HTPA 32x32d': []})
+    first_row = pd.DataFrame({"HTPA 32x32d": []})
     first_row.to_csv(output_fp, index=False, sep=PD_SEP)
-    headers = {'Time (sec)':[],'PTAT':[]}
+    headers = {PD_TIME_COL: [], PD_PTAT_COL: []}
     df = pd.DataFrame(headers)
     for idx in range(np.prod(array.shape[1:])):
-        df.insert(len(df.columns), 'P%04d'%idx, [])
-    # TODO Change separator to , instead of ;
-    df.to_csv(output_fp, mode='a', index=False, sep=PD_SEP)
+        df.insert(len(df.columns), "P%04d" % idx, [])
+    df.to_csv(output_fp, mode="a", index=False, sep=PD_SEP)
 
     for idx in range(array.shape[0]):
-        frame = array[idx,...]
+        frame = array[idx, ...]
         timestamp = timestamps[idx]
         temps = list(frame.flatten())
         row_data = [timestamp, PD_NAN]
         row_data.extend(temps)
         row = pd.DataFrame([row_data])
         row = row.astype(PD_DTYPE)
-        row.to_csv(output_fp, mode='a', header=False, sep=PD_SEP,index=False)
+        row.to_csv(output_fp, mode="a", header=False, sep=PD_SEP, index=False)
     return True
 
+
+def readCsv2np(csv_fp: str):
+    """
+    Read and convert .CSV dataframe to a Heimann HTPA NumPy array shaped [frames, height, width]
+
+    Parameters
+    ----------
+    csv_fp : str
+        Filepath to the csv file tor read.
+
+    Returns
+    -------
+    array : np.array
+        Temperatue distribution sequence, shape [frames, height, width].
+    timestamps : list
+        List of timestamps of corresponding array frames.
+    """
+    df = pd.read_csv(csv_fp, **READ_CSV_ARGS)
+    timestamps = df[PD_TIME_COL]
+    array = df.drop([PD_TIME_COL, PD_PTAT_COL], axis=1).to_numpy(dtype=DTYPE)
+    array = reshapeFlattenedFrames(array)
+    return array, timestamps
+
+
+def applyHeatmap(array, cv_colormap: int = cv2.COLORMAP_JET) -> np.ndarray:
+    """
+    Applies pseudocoloring (heatmap) to a sequence of thermal distribution. Same as np2pc().
+    np2pc() is preffered.
+
+    Parameters
+    ----------
+    array : np.array
+         (frames, height, width)
+    cv_colormap : int, optional
+
+    Returns
+    -------
+    np.array
+         (frames, height, width, channels)
+    """
+    min, max = array.min(), array.max()
+    shape = array.shape
+    array_normalized = (255 * ((array - min) / (max - min))).astype(np.uint8)
+    heatmap_flat = cv2.applyColorMap(array_normalized.flatten(), cv_colormap)
+    return heatmap_flat.reshape([shape[0], shape[1], shape[2], 3])
+
+
+def np2pc(array, cv_colormap: int = cv2.COLORMAP_JET) -> np.ndarray:
+
+    """
+    Applies pseudocoloring (heatmap) to a sequence of thermal distribution. Same as applyHeatmap().
+    np2pc() is preffered.
+
+    Parameters
+    ----------
+    array : np.array
+         (frames, height, width)
+    cv_colormap : int, optional
+
+    Returns
+    -------
+    np.array
+         (frames, height, width, channels)
+    """
+    return applyHeatmap(array, cv_colormap)
+
+
+# TODO heatmap -> pc
+
+
+def saveFrames(array, dir_name: str, extension: str = ".bmp") -> bool:
+    """
+    Exctracts and saves frames from a sequence array into a folder dir_name
+
+    Parameters
+    ----------
+    array : np.array
+         (frames, height, width, channels)
+
+    Returns
+    -------
+    bool
+        True if success
+    """
+    for idx, frame in enumerate(array):
+        cv2.imwrite(os.path.join(dir_name, "%d" % idx + extension), frame)
+    return True
 
 
 def flattenFrames(array):
     """
-    Flattens array of shape (frames, height, width) into array of shape (frames, height*width)
+    Flattens array of shape [frames, height, width] into array of shape [frames, height*width]
 
     Parameters
     ----------
@@ -94,15 +199,66 @@ def flattenFrames(array):
     Returns
     -------
     np.array
-        flattened array
-         (frames, height, width)
+        flattened array (frames, height, width)
     """
     _, height, width = array.shape
-    return array.reshape((-1, height*width))
+    return array.reshape((-1, height * width))
+
+
+def pcWriteGif(array, fp: str, fps=10, loop: int = 0, duration=None):
+    """
+    Converts and saves NumPy array of pseudocolored thermopile sensor array data, shaped [frames, height, width, channels], into a .gif file
+
+    Parameters
+    ----------
+    array : np.array
+        Pseudocolored data (frames, height, width, channels).
+    fp : str
+        The filepath to write to.
+    fps : float, optional
+        Default 10, approx. equal to a typical thermopile sensor array FPS value.
+    loop : int, optional
+        The number of iterations. Default 0 (meaning loop indefinitely).
+    duration : float, list, optional
+        The duration (in seconds) of each frame. Either specify one value
+        that is used for all frames, or one value for each frame.
+        Note that in the GIF format the duration/delay is expressed in
+        hundredths of a second, which limits the precision of the duration. (from imageio doc)
+
+    Returns
+    -------
+    bool
+        True if success.
+    """
+    if not duration:
+        duration = 1 / fps
+    with imageio.get_writer(fp, mode="I", duration=duration, loop=loop) as writer:
+        for frame in array:
+            writer.append_data(frame[:, :, ::-1])
+    return True
+
+
+def timestamps2framedurations(timestamps: list, lastFrameDuration=None) -> list:
+    """
+    # TODO 
+    Produces frame durations list to produce more accurate 
+    # TODO 
+    Parameters
+    ----------
+    array : np.array
+         flattened array (frames, height, width)
+
+    Returns
+    -------
+    np.array
+        reshaped array (frames, height, width)
+    """
+    pass  # TODO
+
 
 def reshapeFlattenedFrames(array):
     """
-    Reshapes array of shape (frames, height*width) into array of shape (frames, height, width)
+    Reshapes array shaped [frames, height*width] into array of shape [frames, height, width]
 
     Parameters
     ----------
@@ -115,16 +271,17 @@ def reshapeFlattenedFrames(array):
         reshaped array (frames, height, width)
     """
     _, elements = array.shape
-    height = int(elements**(1/2))
+    height = int(elements ** (1 / 2))
     width = height
     return array.reshape((-1, height, width))
 
+
 if __name__ == "__main__":
     import os
+
     SAMPLE_FP = os.path.join("testing", "sample.TXT")
     SAMPLE_FP = os.path.join("examples", "person1.TXT")
-    array, timestamps = txt2np(filepath = SAMPLE_FP, array_size = 32)
-    np2df(os.path.join("tmp","try.csv"), array, timestamps)
-    output_fp = os.path.join("tmp","try.csv")
-    frame = array[1,...]
-
+    array, timestamps = txt2np(filepath=SAMPLE_FP, array_size=32)
+    npWriteCsv(os.path.join("tmp", "try.csv"), array, timestamps)
+    output_fp = os.path.join("tmp", "try.csv")
+    frame = array[1, ...]
