68c2296c9c7d8dc4200d347048bbcd512edcfa1d
igor-morawski <igor.morawski@hotmail.com>
2020-02-03 14:16:21 +0800

    repo description

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# htpa32x32d-udp
+This repository contains tools to work with thermopile sensor array Heimann HTPA 32x32d and UDP communication module (included in the starting kit). Thermopile sensor array is an extremely low-resolution far-infrared imaging sensor (camera). This sensor can preserve the privacy of people captured, so I use this sensor in my on-going research on using thermopile sensor arrays in monitoring systems capable of early event detection/risk assessment/accident prevention.
+
+## Learn more
+You can learn about the sensor more by visiting [Heimann Sensor's website][https://www.heimannsensor.com/products_imaging.php], or you can learn more about me and my research [on my website][https://igor-morawski.github.io/].
+
+Feel free to [contact me][https://www.linkedin.com/in/igor-morawski/] if you have any questions about the sensor or the research.
+
+## tools.py
+A collection of useful functions and data structures for working with data captured by Heimann HTPA32x32d and other thermopile sensor arrays. 
+
+## recorder.py
+Python program that connects to Heimann HTPA sensors and records data captured to TXT files. Supports recording mutliple sensors at the same time. This tool is supposed to help developing multi-view thermopile sensor array monitoring system.
+
+## converter.py
+Python program that converts TXT files recorded by Heimann HTPA sensors and 
+
+To get help run (depedning on the OS, you might need to change `python` to `python3`):
+    python converter.py --help
+
+Usage example:
+    python converter.py --csv --gif --bmp --crop=26 --overwrite DIRECTORY_CONTAINING_FILES__OR__SINGLE_FILE_PATH
+Equivalent to:
+    python converter.py -c -g -b --crop=26 --overwrite DIRECTORY_CONTAINING_FILES__OR__SINGLE_FILE_PATH
+
+Explenation:
+Converts all TXT files in a given directory or a given file:
+`--csv` to CSV file,
+`--gif` to GIF annimation preserving original frame durations,
+`--bmp` extracts frames to a directory named after the filename, 
+`--crop` in pixels, data frames are cropped to a patch of a given size in the center of the frame (note: CSV is never affected by this flag),
+`--overwrite` overwrites the files if they already exists.
+
---
+++ converter.py
@@ -0,0 +1,89 @@
+
+
+import argparse
+import glob
+import os
+import tools
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Process .TXT files in a directory passed"
+    )
+    parser.add_argument("object")
+    parser.add_argument(
+        "--gif", "-g", dest="gif", help="Write gifs", action="store_true"
+    )
+    parser.add_argument(
+        "--csv", "-c", dest="csv", help="Write csvs", action="store_true"
+    )
+    parser.add_argument(
+        "--bmp",
+        "-b",
+        dest="bmp",
+        help="Write bitmaps to a directory named same as the processed file",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--crop",
+        dest="crop",
+        help="Crop a rectangular patch of size (crop, crop) from the center of an array",
+        type=int,
+        default=-1
+    )
+    parser.add_argument(
+        "--overwrite",
+        dest="overwrite",
+        help="Overwrite file if it exists",
+        action="store_true",
+    )
+    args = parser.parse_args()
+    dir_path, file_path = None, None
+    if os.path.isdir(args.object):
+        dir_path = os.path.abspath(args.object)
+    elif os.path.isfile(args.object):
+        file_path = os.path.abspath(args.object)
+
+    def txtFunctions(txt_fp, gif=False, csv=False, bmp=False, crop=-1, overwrite=False, **args):
+        def init(txt_fp, ext):
+            parent, txt_fn = os.path.split(txt_fp)
+            fn = txt_fn.split(".TXT")[0]
+            txt_fn = fn + ".TXT"
+            ext_fn = fn + ext
+            ext_fp = os.path.join(parent, ext_fn)
+            if os.path.exists(ext_fp):
+                if not overwrite:
+                    print("{} already exists, aborting conversion".format(ext_fn))
+                    return None
+            print("Converting {} to {}".format(txt_fn, ext_fn))
+            return ext_fp
+
+        array, timestamps = tools.txt2np(txt_fp)
+        # NO CROPPING HERE! CSV SHOULD NOT BE CROPPED!
+        if csv:
+            csv_fp = init(txt_fp, ".csv")
+            if csv_fp:
+                tools.write_np2csv(csv_fp, array, timestamps)
+
+        cropped_array = tools.crop_center(array, crop, crop)
+        if gif:
+            gif_fp = init(txt_fp, ".gif")
+            if gif_fp:
+                pc = tools.np2pc(cropped_array)
+                tools.write_pc2gif(
+                    pc, gif_fp, duration=tools.timestamps2frame_durations(timestamps))
+        if bmp:
+            parent, txt_fn = os.path.split(txt_fp)
+            fn = txt_fn.split(".TXT")[0]
+            dest = fn
+            if init(txt_fp, ""):
+                print("Extracting frames...")
+                tools.save_frames(tools.np2pc(cropped_array), os.path.join(parent, dest))
+        return
+
+    if dir_path:
+        for txt_fp in glob.glob(os.path.join(dir_path, "*.TXT")):
+            txtFunctions(txt_fp, **vars(args))
+    if file_path:
+        txt_fp = file_path
+        txtFunctions(txt_fp, **vars(args))
