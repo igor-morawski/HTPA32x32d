33339b39a00e67086ad489aa394ca30be0cdc444
igor-morawski <igor.morawski@hotmail.com>
2020-04-16 09:04:13 +0800

    typo

--- scripts/MTS2mp4.sh
+++ scripts/MTS2mp4.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 for i in *.MTS; do ffmpeg -i "$i" -c:v copy -c:a aac -strict experimental -b:a 128k "${i%.*}.mp4"; done
--- scripts/gif2mp4.sh
+++ scripts/gif2mp4.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 for i in *.gif; do ffmpeg -i "$i"  -movflags faststart -pix_fmt yuv420p -vf "scale=100*trunc(iw/2)*2:100*trunc(ih/2)*2:flags=neighbor" "${i%.*}.mp4"; done
 
