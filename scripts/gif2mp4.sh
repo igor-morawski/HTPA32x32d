e0447992eca135fdb6c8d5395a36c534749f9512
igor-morawski <igor.morawski@hotmail.com>
2020-05-06 11:19:01 +0800

    gif2mp4 conversion

--- scripts/gif2mp4.sh
+++ scripts/gif2mp4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-for i in *.gif; do ffmpeg -i "$i"  -movflags faststart -pix_fmt yuv420p -vf "scale=100*trunc(iw/2)*2:100*trunc(ih/2)*2:flags=neighbor" "${i%.*}.mp4"; done
+for i in *.gif; do ffmpeg -i "$i" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "${i%.*}.mp4"; done
 
